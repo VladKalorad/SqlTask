6af4f321efa67818874f0bcac0cab80b8a1089c7
VladKalorad <vladchernav210@gmail.com>
2022-10-07 14:11:18 +0300

    init project

---
+++ CreateTable.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE Banking;
+
+GO
+Use Banking
+
+Create table Banks
+(
+	Id INT PRIMARY KEY identity(1,1)
+	Name Varchar(50) not null;
+)
+Create table Affiliates
+(
+	ID INT PRIMARY KEY IDENTITY(1,1)
+	bank_id int references Banks(id);
+	cities_id int references Cities(id);
+)
+Create table Cities 
+(
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	Title VARCHAR(50) not null,
+)
+Create table Accounts
+( 
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	Number Varchar(50) not null,
+	Balance_Overall INT not null,
+	banks_id int references Banks(id),
+	cliens_Id int references Clients(id),
+)
+Create table Clients
+(
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	Firstname varchar(50) not null,
+	LastName varchar(50) not null,
+	social_id int references Social_status(id),
+)
+Create table Social_status
+(
+	ID INT Primary key identity(1,1),
+	Type Varchar(20) not null,
+)
+Create table Cards
+(
+	ID int primary key identity(1,1)
+	Number varchar(16) not null,
+	Cvv int not null,
+	balance money not null,
+	account_id int references Accounts(id),
+)
