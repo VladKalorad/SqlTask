fc3494875de2d9d02713b537b56d081f313b7f6c
VladKalorad <vladchernav210@gmail.com>
2022-10-08 07:56:04 +0300

    Fill in the table

---
+++ FillingTables.sql
@@ -0,0 +1,50 @@
+Use Banking
+
+INSERT Banks VALUES
+('AlfaBank'),
+('Priorbank'),
+('Bnb-bank'),
+('Belarus Bank'),
+('Sberbank')
+
+INSERT Cities VALUES
+('Minsk'),
+('Gomel'),
+('Vitebsk'),
+('Grodno'),
+('Mogilev')
+
+INSERT Affiliates VALUES
+(1,1),
+(2,2),
+(3,3),
+(4,4),
+(5,5)
+
+INSERT Social_status VALUES
+('Student'),
+('Sportsman'),
+('Veteran'),
+('Disabled'),
+('Pensioner')
+
+INSERT Clients VALUES
+('Valery', 'Starozhilov', 1),
+('Ivan', 'Ivanov', 2),
+('Maksim', 'Maksimenko', 3),
+('Alex', 'Alexov', 4),
+('Dima', 'Dimienko', 5)
+
+INSERT Accounts VALUES
+('31702810680062657001', 1233.22,1,1 ),
+('31603810450000000005', 23211.212, 2,2),
+('31102810845370000005', 32412.12, 3,3),
+('51802810657000546001', 40990.111, 4,4),
+('61702840000000001221', 511.22, 5,5)
+
+INSERT Cards VALUES
+('2323242425252626', 123, 12.1,1),
+('1111222233334444', 044, 134.2,1),
+('7070612345658956', 511, 1234.233,1),
+('5675342489891212', 234, 355.34,1),
+('5674872389129843', 199, 4556.334,1)
