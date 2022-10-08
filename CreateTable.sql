122e28d24ec93c4db14a7254df1c6d259a2f30b6
VladKalorad <vladchernav210@gmail.com>
2022-10-08 07:27:59 +0300

    data type change

--- CreateTable.sql
+++ CreateTable.sql
@@ -5,19 +5,31 @@ Use Banking
 
 Create table Banks
 (
-	Id INT PRIMARY KEY identity(1,1)
-	Name Varchar(50) not null;
-)
-Create table Affiliates
-(
-	ID INT PRIMARY KEY IDENTITY(1,1)
-	bank_id int references Banks(id);
-	cities_id int references Cities(id);
+	Id INT PRIMARY KEY identity(1,1),
+	Name Varchar(50) not null,
 )
 Create table Cities 
 (
 	ID INT PRIMARY KEY IDENTITY(1,1),
 	Title VARCHAR(50) not null,
+)
+Create table Affiliates
+(
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	bank_id int references Banks(id),
+	cities_id int references Cities(id),
+)
+Create table Social_status
+(
+	ID INT Primary key identity(1,1),
+	Type Varchar(20) not null,
+)
+Create table Clients
+(
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	Firstname varchar(50) not null,
+	LastName varchar(50) not null,
+	social_id int references Social_status(id),
 )
 Create table Accounts
 ( 
@@ -27,23 +39,11 @@ Create table Accounts
 	banks_id int references Banks(id),
 	cliens_Id int references Clients(id),
 )
-Create table Clients
-(
-	ID INT PRIMARY KEY IDENTITY(1,1),
-	Firstname varchar(50) not null,
-	LastName varchar(50) not null,
-	social_id int references Social_status(id),
-)
-Create table Social_status
-(
-	ID INT Primary key identity(1,1),
-	Type Varchar(20) not null,
-)
 Create table Cards
 (
-	ID int primary key identity(1,1)
+	ID int primary key identity(1,1),
 	Number varchar(16) not null,
 	Cvv int not null,
-	balance money not null,
+	balance float not null,
 	account_id int references Accounts(id),
 )
